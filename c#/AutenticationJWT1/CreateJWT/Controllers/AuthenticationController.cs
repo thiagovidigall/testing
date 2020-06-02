1a9377f8ef674629d1d3c0f69a0d7e46cbd64a36
thiagovidigall <thiagovidigall@gmail.com>
2020-06-02 19:47:55 -0300

    add api with authentication jwt

---
+++ c#/AutenticationJWT1/CreateJWT/Controllers/AuthenticationController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CreateJWT.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CreateJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        // GET: api/<AuthenticationController>
+        [HttpGet]
+        public string Get(string user, string pass)
+        {
+            //return new string[] { "value1", "value2" };
+            if (user == "admin")
+                return AuthenticationConfig.GenerateJSONWebToken(user);
+            else
+                return string.Empty;
+        }
+
+        // POST api/<AuthenticationController>
+        [Authorize]
+        [HttpPost]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IEnumerable<Claim> claim = identity.Claims;
+            var userName = claim.Where(c => c.Type == "UserName").Select(c => c.Value).SingleOrDefault();
+            return "Welcome to " + userName + "!";
+        }
+    }
+}
---
+++ c#/AutenticationJWT1/CreateJWT/Shared/AuthenticationConfig.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateJWT.Shared
+{
+    public static class AuthenticationConfig
+    {
+        public static string GenerateJSONWebToken(string user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("D76F1904209193A5C6234195E995E90CD85CD2235B06C1E237FA94FCCD2D852A"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim("UserName", user),
+                new Claim("Role", "1"),
+            };
+
+            IdentityModelEventSource.ShowPII = true;
+
+            var token = new JwtSecurityToken("http://localhost:51139",
+                "http://localhost:51139",
+                claims,
+                DateTime.Now,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        //ConfigureJwtAuthetication
+
+        internal static TokenValidationParameters tokenValidationParams;
+
+        public static void ConfigureJwtAuthetication(this IServiceCollection services)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("D76F1904209193A5C6234195E995E90CD85CD2235B06C1E237FA94FCCD2D852A"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            tokenValidationParams = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "http://localhost:51139",
+                ValidateLifetime = true,
+                ValidAudience = "http://localhost:51139",
+                RequireSignedTokens = true,
+                //user our signing credentials key here
+                //optionally we can inject an RSA key as
+                //IssuerSigningKey = new RsaSecurityKey(rsaParams),
+                IssuerSigningKey = credentials.Key,
+                ClockSkew = TimeSpan.FromMinutes(30)
+            };
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            .AddJwtBearer(options =>
+             {
+                 options.TokenValidationParameters = tokenValidationParams;
+#if PROD || UAT
+                    options.IncludeErrorDetails = false;
+#elif DEBUG
+                 options.RequireHttpsMetadata = false;
+#endif
+             });
+        }
+    }
+}
